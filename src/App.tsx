5871e9df6bc47c5cf1694d790cdf8e210170df11
Mayeen Ayoub <mayeenayoub@Mayeens-MacBook-Pro.local>
2022-05-08 20:02:10 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Basics from './components/Basics/Basics';
+import Navigation from './components/Navigation/Navigation';
+
+const App = () => {
+  return (
+    <>
+      <Basics />
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
