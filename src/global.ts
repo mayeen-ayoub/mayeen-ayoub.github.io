e00864147acceed09711e61a019d036607e134b9
mayeen-ayoub <mayeen.ayoub@gmail.com>
2024-08-24 16:57:17 -0400

    Update resume

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,7 @@
 export const Globals = {
   GITHUB: 'https://github.com/mayeen-ayoub',
 	LINKEDIN: 'https://www.linkedin.com/in/mayeen-ayoub/',
-	RESUME: 'https://drive.google.com/file/d/1JDG-dyuOeLnUaX0fXPj5L-gzBb2tMqDL/view',
+	RESUME: 'https://drive.google.com/file/d/1JQMzpYNM_1pNfshhaDu2IP2EixejkuP6/view',
 	DEVPOST: 'https://devpost.com/mayeen-ayoub',
 	MAIL_LINK: 'mailto:mayeen.ayoub@gmail.com',
 	MAIL_TEXT: 'mayeen.ayoub@gmail.com'
